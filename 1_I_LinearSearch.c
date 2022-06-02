0b06c164212830067566dde66ada7836b0494e6c
Utsab <utsabbera365@gmail.com>
2022-06-02 23:22:56 +0530

    Added Program 1.I. Linear Search

---
+++ 1_I_LinearSearch.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int linearSearch(int arr[], int size, int val) {
+    for (int i = 0; i < size; i++) {
+        if (arr[i] == val) return i;
+    }
+    return -1;
+}
+
+int main() {
+    int a[] = {1, 5, 3, 2, 6}; //example array
+    int v = 5; //value to be searched
+    int n = sizeof(a)/sizeof(a[0]); //size
+
+    int res = linearSearch(a, n, v); //result : index or -1
+
+    if (res != -1) printf("Index of the value in the array: %d", res);
+    else printf("The value is not present in the array");
+
+    return 0;
+}
